12a12ae8ec6a5163883012c1b149414169a2a038
Alex1899 <kvashilavaaleko@gmail.com>
2021-07-11 22:08:45 +0400

    new ui

--- app/src/main/java/com/example/geofitapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/geofitapp/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.geofitapp.ui
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.geofitapp.R
@@ -8,5 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.elevation = 0F
     }
 }
---
+++ app/src/main/java/com/example/geofitapp/ui/home/HomePageFragment.kt
@@ -0,0 +1,35 @@
+package com.example.geofitapp.ui.home
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.geofitapp.R
+import com.example.geofitapp.databinding.FragmentHomePageBinding
+
+class HomePageFragment : Fragment() {
+    private lateinit var binding: FragmentHomePageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_home_page, container, false)
+
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
